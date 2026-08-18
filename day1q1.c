32f336f857fd5c4d2a5a3ca5c3722928255140e1
vaibhav-1004 <vaibhav1004200810@gmail.com>
2026-08-18 21:55:57 +0530

    Add files via upload

---
+++ day1q1.c
@@ -0,0 +1,10 @@
+/*Write a program to input two numbers and display their sum.*/
+#include <stdio.h>
+int main(){
+    int a,b,sum;
+    printf("Enter two numbers:");
+    scanf("%d%d", &a, &b);
+sum = a + b;
+ printf("%d\n", sum);
+ return 0;
+}
